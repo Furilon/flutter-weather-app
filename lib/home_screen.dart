2833c1c985be6227f837a018b7e42471ddc64426
Mykyta Medvediev <arlekin8881@gmail.com>
2024-05-05 11:53:36 -0700

    Added "Add to Favorites" functionality

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,7 +5,7 @@ import 'weather_data.dart';
 import 'individual_city_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key, required this.title}) : super(key: key);
+  const HomeScreen({super.key, required this.title});
 
   final String title;
 
@@ -19,7 +19,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    // Fetch weather data for major US cities
     fetchWeatherData();
   }
 
@@ -30,14 +29,12 @@ class _HomeScreenState extends State<HomeScreen> {
       Uri url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city,US&appid=$apiKey');
       final response = await http.get(url);
       if (response.statusCode == 200) {
-        // If the server returns a 200 OK response, parse the JSON.
         Map<String, dynamic> data = jsonDecode(response.body);
         WeatherData weatherData = WeatherData.fromJson(data);
         setState(() {
           weatherDataList.add(weatherData);
         });
       } else {
-        // If the server did not return a 200 OK response, throw an exception.
         throw Exception('Failed to load weather data');
       }
     }
@@ -73,12 +70,12 @@ class _HomeScreenState extends State<HomeScreen> {
 class WeatherCard extends StatelessWidget {
   final WeatherData weatherData;
 
-  const WeatherCard({Key? key, required this.weatherData}) : super(key: key);
+  const WeatherCard({super.key, required this.weatherData});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
       child: ListTile(
         title: Text(weatherData.cityName),
         subtitle: Column(
--- lib/individual_city_screen.dart
+++ lib/individual_city_screen.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'weather_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CityForecastScreen extends StatefulWidget {
   final String cityName;
@@ -26,13 +28,11 @@ class _CityForecastScreenState extends State<CityForecastScreen> {
     Uri url = Uri.parse('https://api.openweathermap.org/data/2.5/forecast?q=${widget.cityName}&appid=$apiKey');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      // If the server returns a 200 OK response, parse the JSON.
       List<dynamic> data = jsonDecode(response.body)['list'];
       List<WeatherForecast> forecasts = [];
       DateTime currentDate = DateTime.now();
       for (var item in data) {
         WeatherForecast forecast = WeatherForecast.fromJson(item);
-        // Check if the forecast date is different from the current date
         if (forecast.date.day != currentDate.day) {
           forecasts.add(forecast);
           currentDate = forecast.date;
@@ -40,8 +40,23 @@ class _CityForecastScreenState extends State<CityForecastScreen> {
       }
       return forecasts;
     } else {
-      // If the server did not return a 200 OK response, throw an exception.
       throw Exception('Failed to load forecast data');
+    }
+  }
+
+  void addToFavorites() async {
+    try {
+      await FirebaseFirestore.instance.collection('favorites').add({
+        'user': FirebaseAuth.instance.currentUser?.uid,
+        'cityName': widget.cityName,
+      });
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Added to favorites'),
+      ));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Failed to add to favorites'),
+      ));
     }
   }
 
@@ -57,21 +72,31 @@ class _CityForecastScreenState extends State<CityForecastScreen> {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               List<WeatherForecast> forecasts = snapshot.data!;
-              return ListView.builder(
-                itemCount: forecasts.length,
-                itemBuilder: (context, index) {
-                  WeatherForecast forecast = forecasts[index];
-                  return ListTile(
-                    title: Text('${forecast.date.toLocal()}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Temperature: ${forecast.temperature.toStringAsFixed(1)}°F'),
-                        Text('Weather: ${forecast.weatherDescription}'),
-                      ],
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: forecasts.length,
+                      itemBuilder: (context, index) {
+                        WeatherForecast forecast = forecasts[index];
+                        return ListTile(
+                          title: Text('${forecast.date.toLocal()}'),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Temperature: ${forecast.temperature.toStringAsFixed(1)}°F'),
+                              Text('Weather: ${forecast.weatherDescription}'),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
+                  ),
+                  ElevatedButton(
+                    onPressed: addToFavorites,
+                    child: const Text('Add to Favorites'),
+                  ),
+                ],
               );
             } else if (snapshot.hasError) {
               return Text('Error: ${snapshot.error}');
@@ -83,5 +108,3 @@ class _CityForecastScreenState extends State<CityForecastScreen> {
     );
   }
 }
-
-
