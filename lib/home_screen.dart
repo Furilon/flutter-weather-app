a6a4bb2fdc1bd672f776f2598b2d86867d166603
Mykyta Medvediev <arlekin8881@gmail.com>
2024-05-04 18:55:11 -0700

    Added fetch for 5 major us cities functionality as homescreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -19,71 +12,86 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
+  List<WeatherData> weatherDataList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    // Fetch weather data for major US cities
+    fetchWeatherData();
+  }
+
+  Future<void> fetchWeatherData() async {
+    List<String> cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix'];
+    String apiKey = 'fd80244eb438a5e523f948e21723406f';
+    for (String city in cities) {
+      Uri url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city,US&appid=$apiKey');
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        // If the server returns a 200 OK response, parse the JSON.
+        Map<String, dynamic> data = jsonDecode(response.body);
+        WeatherData weatherData = WeatherData.fromJson(data);
+        setState(() {
+          weatherDataList.add(weatherData);
+        });
+      } else {
+        // If the server did not return a 200 OK response, throw an exception.
+        throw Exception('Failed to load weather data');
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+      body: ListView.builder(
+        itemCount: weatherDataList.length,
+        itemBuilder: (context, index) {
+          return WeatherCard(weatherData: weatherDataList[index]);
+        },
+      ),
+    );
+  }
+}
+
+class WeatherData {
+  final String cityName;
+  final double temperature;
+  final String weatherDescription;
+
+  WeatherData({required this.cityName, required this.temperature, required this.weatherDescription});
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    return WeatherData(
+      cityName: json['name'],
+      temperature: (json['main']['temp'] - 273.15) * 9 / 5 + 32, // Convert temperature from Kelvin to Fahrenheit
+      weatherDescription: json['weather'][0]['description'],
+    );
+  }
+}
+
+class WeatherCard extends StatelessWidget {
+  final WeatherData weatherData;
+
+  const WeatherCard({Key? key, required this.weatherData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        title: Text(weatherData.cityName),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Temperature: ${weatherData.temperature.toStringAsFixed(1)}°F'),
+            Text('Weather: ${weatherData.weatherDescription}'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
